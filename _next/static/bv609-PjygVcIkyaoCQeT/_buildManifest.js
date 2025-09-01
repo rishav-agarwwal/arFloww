8212179f05b2f8b31ed127e683f5f0c3a2da0c27
Rishav Agarwwal <rishav.agarwal@outlook.in>
2025-09-01 06:05:49 +0530

    Delete _next/static directory
    Signed-off-by: Rishav Agarwwal <rishav.agarwal@outlook.in>

--- _next/static/bv609-PjygVcIkyaoCQeT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/235-6d77eadf203bb383.js","static/chunks/pages/index-376248910e209c25.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
